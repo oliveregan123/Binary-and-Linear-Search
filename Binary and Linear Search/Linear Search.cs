5a36c8bf25dc77efbb1fcb1b8e5a5f38e3405f45
Oliver Egan <K31724@eltham-college.org.uk>
2021-01-07 14:40:03 +0000

    done

--- Binary and Linear Search/Linear Search.cs
+++ Binary and Linear Search/Linear Search.cs
@@ -11,11 +11,13 @@ namespace Binary_and_Linear_Search
             int[] x = new int [] { 1, 3, 6, 7, 10, 16, 19, 300, 302 };
             Console.WriteLine("what number are you looking for?");
             n = Convert.ToInt32(Console.ReadLine());
-            for (int y = 0; y > x.Length - 1; y = y + 1)
+            bool numfound = false;
+            while (numfound == false)
             {
                 if (n == x[y])
                 {
                     Console.WriteLine("Your number is" + x[y]);
+                    numfound == true;
                 }
                 
                 
